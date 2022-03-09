89cea4195e14b07b4a1b66bfe3ccb4bfbdc07a68
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2022-03-09 18:01:37 +0200

    Initial Commit

---
+++ Challenge45UITests/Challenge45UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Challenge45UITestsLaunchTests.swift
+//  Challenge45UITests
+//
+//  Created by Adonis Rumbwere on 9/3/2022.
+//
+
+import XCTest
+
+class Challenge45UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
