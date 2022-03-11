8a20bd42c570a7a840ac7c9de5f5b979b45826a9
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2022-03-11 09:09:52 +0200

    Handling http response errors

--- Challenge45/Networking/AppError.swift
+++ Challenge45/Networking/AppError.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+enum AppError: LocalizedError {
+    case errorDecoding
+    case unknownError
+    case invalidUrl
+    case serverError(String)
+    
+    
+    var errorDescription: String? {
+        switch self {
+            case .errorDecoding:
+                return "Response could not be decoded"
+            case . unknownError:
+                return "Unknow Error"
+            case .invalidUrl:
+                return "URL is invalid"
+            case .serverError(let error):
+                return error
+        }
+    }
+
+}
