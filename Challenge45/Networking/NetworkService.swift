88aeeefcbf011955b7a127ee6b82ba649e33c0bd
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2022-03-11 15:37:19 +0200

    managed to get the response back, now we can decode it

--- Challenge45/Networking/NetworkService.swift
+++ Challenge45/Networking/NetworkService.swift
@@ -10,6 +10,45 @@ import FileProvider
 
 struct NetworkService {
     
+    static let shared = NetworkService()
+    
+    private init() {}
+    
+    func myfirstRequest(){
+        
+        request(route: .dailyWeather, method: .get, type: String.self, completion: { _ in })
+       
+    }
+    
+    
+    
+    private func request<T: Codable>(route: Route,
+                                     method: Method,
+                                     parameters: [String: Any]? = nil,
+                                     type: T.Type,
+                                     completion: (Result<T, Error>) -> Void) {
+       guard let request = createRequest(route: route, method: method, parameters: parameters) else {
+            completion(.failure(AppError.unknownError))
+            return
+        }
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            var result: Result<Data, Error>?
+            if let data = data {
+                result = .success(data)
+                let responseString = String(data: data, encoding: .utf8) ?? "Could not stringify the data"
+                print("The response  is :\n\(responseString)")
+            } else if let error = error {
+                result = .failure(error)
+                print("The error is: \(error.localizedDescription)")
+            }
+            DispatchQueue.main.async {
+             //TODO
+            }
+            
+        }.resume()
+        
+    }
+    
     /// The function is generating a urlRequest
     /// - Parameters:
     ///   - route: the specific endpoint/path to the resource in the backend
@@ -25,6 +64,7 @@ struct NetworkService {
         var urlRequest = URLRequest(url: url)
         urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
         urlRequest.httpMethod = method.rawValue
+        urlRequest.addValue("9e43721cd8mshcda865306926f66p103b83jsnc7e578350692", forHTTPHeaderField: "x-rapidapi-key")
         
         if let params = parameters {
             switch method {
@@ -33,7 +73,6 @@ struct NetworkService {
                     var urlComponent = URLComponents(string: urlString)
                     urlComponent?.queryItems = params.map {URLQueryItem(name: $0, value: "\($1)") }
                     urlRequest.url = urlComponent?.url
-                    
             }
         }
         return urlRequest
