da0fa377a7cd0a4d12dbd3b84661ab76d2a692fe
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2022-03-11 18:27:28 +0200

    Temperature Modell

--- Challenge45/Model/Temp.swift
+++ Challenge45/Model/Temp.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Temperature: Codable {
-    var min: Double
-    var max: Double
+struct Temp: Codable {
+    let day, min, max, night: Double?
+    let eve, morn: Double?
 }
