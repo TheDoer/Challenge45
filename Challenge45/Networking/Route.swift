dd7b5ec2ff6f882a1bb207a26519308ce42950ba
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2022-03-11 09:11:35 +0200

    handle open weather endpoints with routes

--- Challenge45/Networking/Route.swift
+++ Challenge45/Networking/Route.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+enum Route {
+    static let baseUrl = "https://community-open-weather-map.p.rapidapi.com"
+    
+    case dailyWeather
+    
+    var description: String {
+        switch self {
+            case .dailyWeather:
+                return "/forecast/daily?q=harare%2Czw&units=imperial&id=Harare"
+        }
+    }
+    
+}
+
+
