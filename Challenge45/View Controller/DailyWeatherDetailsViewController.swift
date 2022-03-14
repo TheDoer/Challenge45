05f6fc74d10d9ef0deb7f018b798e4e4b9af12a1
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2022-03-14 08:22:38 +0200

    added cellForRowAt func

--- Challenge45/View Controller/DailyWeatherDetailsViewController.swift
+++ Challenge45/View Controller/DailyWeatherDetailsViewController.swift
@@ -8,22 +8,41 @@
 import UIKit
 
 class DailyWeatherDetailsViewController: UIViewController {
-
+    
+    @IBOutlet weak var cityLbl: UILabel!
+    @IBOutlet weak var tempLbl: UILabel!
+    @IBOutlet weak var weatherDescriptionLbl: UILabel!
+    @IBOutlet weak var tempHighLbl: UILabel!
+    @IBOutlet weak var windLbl: UILabel!
+    @IBOutlet weak var humidityLbl: UILabel!
+    @IBOutlet weak var sunriseLbl: UILabel!
+    @IBOutlet weak var sunsetLbl: UILabel!
+    
+    var dailyWeatherDetails: List?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        populateWeatherDetails()
 
-        // Do any additional setup after loading the view.
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func populateWeatherDetails(){
+        
+        tempLbl.text = "\(dailyWeatherDetails?.temp?.day ?? 0)°"
+        weatherDescriptionLbl.text = dailyWeatherDetails?.weather?[0].weatherDescription
+        tempHighLbl.text = "\(dailyWeatherDetails?.temp?.max ?? 00 )°  /  \(dailyWeatherDetails?.temp?.min ?? 00)°"
+        windLbl.text = "\(dailyWeatherDetails?.speed ?? 0.0) km/h"
+        humidityLbl.text = "\(dailyWeatherDetails?.humidity ?? 0) %"
+        
+        
+        
+        
+        
     }
-    */
+    
 
 }
--- Challenge45/View Controller/DailyWeatherViewController.swift
+++ Challenge45/View Controller/DailyWeatherViewController.swift
@@ -9,46 +9,36 @@ import UIKit
 
 class DailyWeatherViewController: UIViewController {
     
-    
     @IBOutlet weak var tableView: UITableView!
-    
     
     var allDailyWeather: [List] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerCells()
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         NetworkService.shared.fetchDailyWeatherData { [weak self] (result) in
             switch result {
                 case .success(let dailyWeatherData):
                     print("It was successful")
                     self?.allDailyWeather = dailyWeatherData
+                    //self?.allDailyWeather.append(contentsOf: dailyWeatherData)
                     //print("All Data: \(self?.allDailyWeather)")
+                    self?.tableView.reloadData()
                 case .failure(let error):
                     print("The error is: \(error.localizedDescription)")
                     
             }
             
-            print("Weather Data Count: \(self?.allDailyWeather.count)")
         }
-        
-        
-        
-        
-//        NetworkService.shared.myfirstRequest { (result) in
-//            switch result {
-//                case .success(let data):
-//                    //print("The decoded data is: \(data)")
-//                    for dailyWeather in data {
-//                        print(dailyWeather.weather ?? "")
-//                    }
-//
-//
-//                case .failure(let error):
-//                    print("The error is: \(error.localizedDescription)")
-//            }
-//        }
-        
+    }
+    
+    
+    private func registerCells() {
+        tableView.register(UINib(nibName: DailyWeatherTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: DailyWeatherTableViewCell.identifier)
     }
 
 
@@ -57,14 +47,27 @@ class DailyWeatherViewController: UIViewController {
 extension DailyWeatherViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        allDailyWeather.count
+        return self.allDailyWeather.count
+        
+        
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: DailyWeatherTableViewCell.identifier) as! DailyWeatherTableViewCell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: DailyWeatherTableViewCell.identifier, for: indexPath) as! DailyWeatherTableViewCell
         cell.setup(dailyWeatherCellData: allDailyWeather[indexPath.row])
+       
         return cell
         
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        let controller = DailyWeatherDetailsViewController.instantiate()
+        controller.dailyWeatherDetails = allDailyWeather[indexPath.row]
+        navigationController?.pushViewController(controller, animated: true)
+        
+    }
+    
     
 }
