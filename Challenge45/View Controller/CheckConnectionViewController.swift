40d778b106a6149fd809ac867931c8bd4d1863c6
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2022-03-14 14:52:36 +0200

    added checking network functionality

---
+++ Challenge45/View Controller/CheckConnectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CheckConnectionViewController.swift
+//  Challenge45
+//
+//  Created by Adonis Rumbwere on 14/3/2022.
+//
+
+import UIKit
+
+class CheckConnectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
