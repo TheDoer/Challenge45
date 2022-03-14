676bb74d437667889707f9ebeec783b7d938267f
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2022-03-14 07:46:36 +0200

    added an extension that instantiate a vc from the main SB

--- Challenge45/Extensions/UIViewController+Extension.swift
+++ Challenge45/Extensions/UIViewController+Extension.swift
@@ -5,4 +5,18 @@
 //  Created by Adonis Rumbwere on 12/3/2022.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    static var identifier: String {
+        return String(describing: self)
+        
+    }
+    
+    static func instantiate() -> Self {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        return storyboard.instantiateViewController(withIdentifier: identifier) as! Self
+    }
+    
+}
